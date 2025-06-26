9a15b813375f4a79c8b4cf0e542de43937c6a877
BielDias2501 <gabrieldiascl@gmail.com>
2025-06-26 17:07:38 -0300

    Add sum function and corresponding tests; initialize package.json and .gitignore

---
+++ AULA02/src/test/sum.test.js
@@ -0,0 +1,14 @@
+const sum = require ('../modules/sum'); //importação do arquivo/modulo
+
+describe('Testes básicos da função soma', () => {
+    test('Verificar, se a soma de números positivos', ()=>{
+    expect(sum(8,8)).toBe(16);
+})
+
+    test('Verificar, se a soma de números negativos', ()=>{
+    expect(sum(-8, -8)).toBe(-16);
+})
+    test('Verificar, se a soma de números com texto', ()=>{
+    expect(sum(-8, '8')).toBe("Erro não é possivel realizar a soma");
+})
+})
