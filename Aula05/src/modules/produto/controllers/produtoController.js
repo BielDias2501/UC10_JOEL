41a26dd3b05841a1f48c36bdff740ebb7119bb1f
BielDias2501 <gabrieldiascl@gmail.com>
2025-07-17 17:22:50 -0300

    feat: Implementar validações e testes para criação de produtos

--- Aula05/src/modules/produto/controllers/produtoController.js
+++ Aula05/src/modules/produto/controllers/produtoController.js
@@ -2,28 +2,23 @@ const produtoModel = require('../models/produtoModel');
 
 class ProdutoController {
 
-    static async criar(req, res) {
-        try {
-            const { nome, preco, estoque } = req.body;
-
-            if (nome && preco && estoque) {
-                const produto = produtoModel.create({ nome, preco, estoque });
-                res.status(201).json({ mensagem: 'Aluno cadastrado com sucesso', aluno })
-            } else {
-                res.status(400).json({ mensagem: 'Todos os campos devem ser preenchidos' })
-            }
-        } catch (error) {
-            res.status(500).json({ erro: error.message });
-        }
-
-
-
-        return produto
-    }
     static async criarProduto(dados) {
         const { nome, preco, estoque } = dados.body;
-        if (!nome || !preco || !estoque) {
+     
+        if (!nome || !preco || preco===0 || !estoque || estoque===0) {
             throw new Error('Todos os campos devem ser preenchidos');
+        }
+        if (typeof nome !== 'string') {
+            throw new Error('Nome inválido');
+        }
+        if (typeof preco !== 'number' || preco<0) {
+            throw new Error('Preço inválido');
+        }
+        if (typeof estoque !== 'number' || estoque<0) {
+            throw new Error('Estoque inválido');
+        }
+        if (typeof preco !== 'number' || preco<0) {
+            throw new Error('Preço inválido');
         }
         return await produtoModel.create({ nome, preco, estoque });
     }
@@ -32,3 +27,6 @@ class ProdutoController {
 }
 
 module.exports = ProdutoController;
+
+
+
